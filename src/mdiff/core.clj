96dc429db210e147928e261ceae9667137c34ebd
Alex W <alex@blinkenlights.ca>
2018-01-03 15:10:59 -0500

    Initial commit

---
+++ src/mdiff/core.clj
@@ -0,0 +1,6 @@
+(ns mdiff.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/mdiff/core_test.clj
@@ -0,0 +1,7 @@
+(ns mdiff.core-test
+  (:require [clojure.test :refer :all]
+            [mdiff.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
